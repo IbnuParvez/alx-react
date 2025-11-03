d666454f29b121419707ba7a77264e1743d3fbc0
IbnuParvez <parvezfatma98@gmail.com>
2025-11-03 14:23:28 +0300

    fundamental mongosh

--- mern-stack/wk-1/crud.js
+++ mern-stack/wk-1/crud.js
@@ -5,12 +5,20 @@ const { Task } = require('./models/task');
 async function main(){
 	await connectDB();
 
-	const john = await User.create({ name: "John onyango", email: "john,plp"});
-	console.log("Created", john);
+	//const john = await User.create({ name: "John onyango", email: "john,plp"});
+	//console.log("Created", john);
 
-	const users = await User.find().select("name email");
-	console.log("Users", users);
+	//const users = await User.find().select("name email");
+	//console.log("Users", users);
 	console.log("All users are ok");
+	
+	//await User.deleteOne({ name: "John onyango", email: "john,plp" });
+	//console.log("JOhn gone");
+	
+	const byStatus = await Task.aggregate([
+		{ $group: { _id: "$status", total: { $sum : 1 } } }
+	]);
+	console.log("Task status", byStatus);
 
 	await mongoose.disconnect();
 }
