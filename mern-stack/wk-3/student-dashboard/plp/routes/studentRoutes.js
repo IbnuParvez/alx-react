f39470c294a3267416344ea3bc6f2dabfb95269f
IbnuParvez <parvezfatma98@gmail.com>
2025-11-11 16:07:44 +0300

    edited server

---
+++ mern-stack/wk-3/student-dashboard/plp/routes/studentRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const Student = require('../models/student');
+
+router.get('/', async (req, res) => {
+	try {
+		const students = await Student.find();
+		res.json(students);
+	} catch (error) {
+		res.status(500).json( { message: error.message });
+	}
+});
+
+router.post('/', async(req, res) => {
+	const { name, age, email } = req.body;
+	try {
+		const student = new Student({ name, age, email});
+		const saved = await student.save();
+		res.status(201).json(saved);
+	} catch (error) {
+		res.status(400).json({ message: error.message }); 
+	}
+});
+
+router.put('/:id', async(req, res) => {
+	try {
+		const student = await Student.findByIdAndUpdate(req.params.id, req.body, { new: true });
+		res.json(student);
+	} catch (error) {
+		res.status(400).json({ message: error.message});
+	}
+});
+
+router.delete('/:id', async(req, res) =>  {
+	try {
+	await Student.findByIdAndDelete(req.params.id);
+	res.json({ message: "Student Deleted"});
+	} catch (error) {
+		res.status(500).json({ message: error.message});
+	}
+});
+
+module.exports = router;
---
+++ mern-stack/wk-3/student-dashboard/plp/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cors({origin: "http://localhost:5173",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true
+}));
+
+connectDB();
+
+app.use('/students', require('./routes/studentRoutes'));
+
+app.get('/', (req, res) => { 
+	res.send("Api server is up and running");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
