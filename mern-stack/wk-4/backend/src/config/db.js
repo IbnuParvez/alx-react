9b2e6102a3b71252e95bc2d89c96e913052a8185
IbnuParvez <parvezfatma98@gmail.com>
2025-11-15 15:41:48 +0300

    problems problems!!!

--- mern-stack/wk-4/backend/src/config/db.js
+++ mern-stack/wk-4/backend/src/config/db.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 
 async function connectDB(){
   try {
-    await mongoose.connect(process.env.MONGO_URI); 
-    console.log("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      connectTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    }); 
+    console.log("✅ MongoDB connected");
   } catch (error) {
-    console.error("Mongo error: " ,error.message);
+    console.error("❌ Mongo error: " ,error.message);
     process.exit(1);
   }
 }
